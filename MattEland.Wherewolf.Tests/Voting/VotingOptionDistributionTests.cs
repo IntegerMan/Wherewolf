08e9b20590fb4cc58fb1f7385c31ea82d0338441
Matt Eland <Matt.Eland@GMail.com>
2024-07-10 01:53:48 -0400

    Additional permutation counts

--- MattEland.Wherewolf.Tests/Voting/VotingOptionDistributionTests.cs
+++ MattEland.Wherewolf.Tests/Voting/VotingOptionDistributionTests.cs
@@ -20,4 +20,39 @@ public class VotingOptionDistributionTests
         // Assert
         permutations.Count.ShouldBe(27);
     }
+    
+    [Fact]
+    public void VotingOptionsWith4PlayersShouldHaveCorrectPermutationCount()
+    {
+        // Arrange
+        GameSetup setup = new();
+        setup.AddPlayers(new Player("A", new RandomController()), 
+            new Player("B", new RandomController()), 
+            new Player("C", new RandomController()),
+            new Player("D", new RandomController()));
+
+        // Act
+        var permutations = setup.GetVotingPermutations().ToList();
+
+        // Assert
+        permutations.Count.ShouldBe(256);
+    }    
+    
+    [Fact]
+    public void VotingOptionsWith5PlayersShouldHaveCorrectPermutationCount()
+    {
+        // Arrange
+        GameSetup setup = new();
+        setup.AddPlayers(new Player("A", new RandomController()), 
+            new Player("B", new RandomController()), 
+            new Player("C", new RandomController()),
+            new Player("D", new RandomController()),
+            new Player("E", new RandomController()));
+
+        // Act
+        var permutations = setup.GetVotingPermutations().ToList();
+
+        // Assert
+        permutations.Count.ShouldBe(3125);
+    }    
 }
