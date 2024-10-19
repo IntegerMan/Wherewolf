664f6964499f4e91c3dbc2d0676804e9c8ec98d0
Matt Eland <Matt.Eland@GMail.com>
2024-10-18 21:35:09 -0400

    Working on optimal claim calculations

--- MattEland.Wherewolf.Console/HumanController.cs
+++ MattEland.Wherewolf.Console/HumanController.cs
@@ -34,21 +34,29 @@ public class HumanController : PlayerController
 
     public override Player GetPlayerVote(Player votingPlayer, GameState gameState)
     {
+        Dictionary<Player, float> probs = VotingHelper.GetVoteVictoryProbabilities(votingPlayer, gameState);
+        
         SelectionPrompt<Player> prompt = new();
         prompt.Title("Who are you voting for?");
         prompt.AddChoices(gameState.Players.Where(p => p != votingPlayer));
-        prompt.Converter = p => p.GetPlayerMarkup();
+        prompt.Converter = p => $"{p.GetPlayerMarkup()} ({probs[p]:P2} likely to result in a win)";
 
         return AnsiConsole.Prompt(prompt);
     }
 
     public override GameRole GetInitialRoleClaim(Player player, GameState gameState)
     {
+        
         SelectionPrompt<GameRole> prompt = new();
         prompt.Title("What role are you claiming you started as? (Actual: " + gameState.GetStartRole(player).AsMarkdown() + ")");
         prompt.AddChoices(gameState.Roles.Distinct());
         prompt.HighlightStyle(new Style(foreground: Color.White));
-        prompt.Converter = r => $"{r.AsMarkdown()} ({RoleClaimVotingProbabilities.CalculateAverageBeliefProbability(gameState, player, r):P2} Likely to be believed)";
+        prompt.Converter = r =>
+        {
+            float winProb = VotingHelper.GetAssumedStartRoleVictoryProbabilities(player, gameState, r);
+                
+            return $"{r.AsMarkdown()} ({RoleClaimVotingProbabilities.CalculateAverageBeliefProbability(gameState, player, r):P2} Likely to be believed, {winProb:P2} probable to win)";
+        };
         
         return AnsiConsole.Prompt(prompt);
     }
--- MattEland.Wherewolf/Probability/VotingHelper.cs
+++ MattEland.Wherewolf/Probability/VotingHelper.cs
@@ -1,5 +1,6 @@
 using MattEland.Wherewolf.Events;
 using MattEland.Wherewolf.Phases;
+using MattEland.Wherewolf.Roles;
 
 namespace MattEland.Wherewolf.Probability;
 
@@ -24,6 +25,60 @@ public static class VotingHelper
         }
 
         return winProbability;
+    }        
+    
+    public static bool DoesPlayerWinWithBestVotingOptions(Player player, GameState state)
+    {
+        Dictionary<Player, Player> votes = new();
+        Random rand = new();
+        foreach (var votingPlayer in state.Players)
+        {
+            Dictionary<Player, float> probs = GetVoteVictoryProbabilities(votingPlayer, state);
+            var bestOptions = probs.Where(kvp => Math.Abs(kvp.Value - probs.Values.Max()) < double.Epsilon).ToList();
+            var bestOption = bestOptions.Count == 1 ? bestOptions[0] : bestOptions.MinBy(_ => rand.Next());
+            
+            votes[votingPlayer] = bestOption.Key;
+        }
+        
+        Dictionary<Player, int> voteTotals = GetVotingResults(votes);
+        
+        GameResult gameResult = state.DetermineGameResults(voteTotals);
+        
+        return gameResult.WinningPlayers.Contains(player);
+    }    
+    
+    public static float GetAssumedStartRoleVictoryProbabilities(Player player, GameState state, GameRole role)
+    {
+        // Identify all starting game states where the player is the role
+        List<GameState> permutations = state.Setup.GetSetupPermutations()
+                                                  .Where(s => s.GetStartRole(player) == role)
+                                                  .ToList();
+        
+        // Remove ones where other players started as roles the player knows they couldn't have started as
+        PlayerProbabilities probabilities = state.CalculateProbabilities(player);
+        foreach (var otherPlayer in state.Players.Where(p => p != player))
+        {
+            foreach (var prob in probabilities.GetStartProbabilities(state.GetPlayerSlot(otherPlayer)))
+            {
+                if (prob.Value <= 0f)
+                {
+                    permutations = permutations.Where(s => s.GetStartRole(otherPlayer) != prob.Key).ToList();
+                }
+            }
+        }
+
+        // Given these roles, assume the player voted for the person who gives them the highest win probability (randomize ties)
+        int wins = 0;
+        foreach (var perm in permutations)
+        {
+            if (DoesPlayerWinWithBestVotingOptions(player, perm))
+            {
+                wins++;
+            }
+        }
+
+        // Return the average win probability for the player
+        return wins / (float)permutations.Count;
     }
 
     public static List<GameState> GetPossibleGameStatesForPlayer(Player player, GameState state)
@@ -89,14 +144,9 @@ public static class VotingHelper
         float maxProb = probabilities.Max(kvp => kvp.Value);
 
         List<KeyValuePair<Player, float>> atMax = probabilities.Where(kvp => kvp.Value >= maxProb).ToList();
-        
-        if (atMax.Count > 1)
-        {
-            return atMax[randomizer.Next(atMax.Count)].Key;
-        }
-        else
-        {
-            return atMax.First().Key;
-        }
+
+        return atMax.Count == 1 
+            ? atMax.First().Key 
+            : atMax[randomizer.Next(atMax.Count)].Key;
     }
 }
