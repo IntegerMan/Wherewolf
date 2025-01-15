3a20b7fe252fef4d5f1b1a25c035434e33d73bad
Matt Eland <Matt.Eland@GMail.com>
2025-01-14 22:54:56 -0500

    Added Roles List

--- MattEland.Wherewolf.BlazorFrontEnd/Program.cs
+++ MattEland.Wherewolf.BlazorFrontEnd/Program.cs
@@ -1,13 +1,15 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MattEland.Wherewolf.BlazorFrontEnd;
+using MattEland.Wherewolf.Services;
 using MudBlazor.Services;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
+WebAssemblyHostBuilder builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
+builder.Services.AddScoped<RoleService>();
 
 await builder.Build().RunAsync();
---
+++ MattEland.Wherewolf.Services/RoleService.cs
@@ -0,0 +1,16 @@
+﻿using MattEland.Wherewolf.Roles;
+
+namespace MattEland.Wherewolf.Services;
+
+public class RoleService
+{
+    public string GetDescription(GameRole role) 
+        => role switch
+        {
+            GameRole.Villager => "Villagers are on the villager team and do not get any special abilities or additional information.",
+            GameRole.Werewolf => "Werewolves are trying not to get caught. They know who each other are. If a werewolf is alone, they get to see a card from the center.",
+            GameRole.Insomniac => "The Insomniac wakes up last and gets to see their card.",
+            GameRole.Robber => "The Robber gets to swap cards with another player and then look at their new card.",
+            _ => $"Unknown role: {role}"
+        };
+}
