8a931b452cbdab0969fdbe50d480093ed536bc07
Matt Eland <Matt.Eland@GMail.com>
2025-03-08 15:27:37 -0500

    Timeline rendering of specific role claims

---
+++ MattEland.Wherewolf.BlazorFrontEnd/Components/RoleIndicator.razor.cs
@@ -0,0 +1,10 @@
+using MattEland.Wherewolf.Roles;
+using Microsoft.AspNetCore.Components;
+
+namespace MattEland.Wherewolf.BlazorFrontEnd.Components;
+
+public partial class RoleIndicator : ComponentBase
+{
+    [Parameter]
+    public required GameRole Role { get; set; }
+}
